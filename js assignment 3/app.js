e3ee279cddda6198ed54b17b1bbaf33ad970d39e
Syed Abdul Samad Tirmizi <144122288+samadtirmizi7@users.noreply.github.com>
2023-09-15 11:34:44 +0500

    Add files via upload

---
+++ js assignment 3/app.js
@@ -0,0 +1,22 @@
+// task 1
+var studentNames = [];
+// task 2 
+var studentData = {
+studentNames: []
+};
+// task 3
+var colors = ["red", "green", "blue", "yellow"];
+// task 4
+var numbers = [1, 2, 3, 4, 5];
+// task 5
+var booleanArray = [true, false, true];
+// task 6
+let mixedArray = ["apple", 42, true];
+// task 7
+var arr1 = ["SSC", "HSC", "BCS","BS", "BCOM" , "MS" , "M. Phil" , "PhD"]
+{
+    document.write("Qualification:" , "<br>" , arr1[0] , "<br>" , arr1[1] , "<br>" , arr1[2] , "<br>" , arr1[3] , "<br>" , arr1[4] , "<br>" , arr1[5] , "<br>" , arr1[6] , "<br>" , arr1[7] )
+}
+// task 8
+var arrStu = ["bilal" , "shahzaib" , "samad"];
+  
